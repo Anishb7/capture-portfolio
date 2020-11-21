81b9c6fa30ab92cff4b344c713436e9640301d91
Anishb7 <anishbhattaraicsit2014@gmail.com>
2020-11-21 19:03:03 +0545

    12. About animation

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,24 +1,25 @@
-import React from 'react';
-//Page components
-import AboutSection from '../components/AboutSection';
-import ServicesSection from '../components/ServicesSection';
-import FaqSection from '../components/FaqSection';
-
+import React from "react";
+//Page Components
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 //Animations
-import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
+import ScrollTop from "../components/ScrollTop";
 
 const AboutUs = () => {
   return (
     <motion.div
-      exit='exit'
+      exit="exit"
       variants={pageAnimation}
-      initial='hidden'
-      animate='show'
+      initial="hidden"
+      animate="show"
     >
-      <AboutSection />;
+      <AboutSection />
       <ServicesSection />
       <FaqSection />
+      <ScrollTop />
     </motion.div>
   );
 };
